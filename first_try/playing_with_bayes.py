6c460eecd22d0f47c026694884791f82e6f2695e
Maturrin <migue.solinas@gmail.com>
2018-05-13 22:17:18 +0200

    First

---
+++ first_try/playing_with_bayes.py
@@ -0,0 +1,13 @@
+from thinkbayes import Pmf
+
+#Cookies problem
+pmf = Pmf()
+pmf.Set('Bowl 1', 0.5)
+pmf.Set('Bowl 2', 0.5)
+
+pmf.Mult('Bowl 1', 0.75)
+pmf.Mult('Bowl 2', 0.5)
+
+pmf.Normalize()
+
+print (pmf.Prob('Bowl 1'))
